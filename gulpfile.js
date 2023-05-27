e0c5084f20cfbfcb5a22cada9a0811a1d7d1b988
sergentors705 <sergentors705@gmail.com>
2023-05-27 20:24:04 +0600

    added project builder

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+import gulp from 'gulp';
+import htmlmin from 'gulp-htmlmin';
+import plumber from 'gulp-plumber';
+import sass from 'gulp-dart-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import browser from 'browser-sync';
+
+//Html
+
+export const html = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin())
+  .pipe(gulp.dest('build/'))
+}
+
+// Styles
+
+export const styles = () => {
+  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+    .pipe(browser.stream());
+}
+
+// Server
+
+const server = (done) => {
+  browser.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/*.html').on('change', () => {gulp.series(html)(); browser.reload();});
+}
+
+export default gulp.series(
+  html, styles, server, watcher
+);
